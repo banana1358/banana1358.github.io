1c51ff68178cbf73483cd317e414b8f746bc86fc
deannayee-0108 <64994919+deannayee-0108@users.noreply.github.com>
2020-06-20 13:15:58 -0700

    add files

---
+++ password.js
@@ -0,0 +1,8 @@
+function passWord() {
+  var password = prompt("Please Enter the password")
+  if(password == "password"){
+    alert("Welcome to the memories")
+    window.open("memory.html")
+    }
+  }
+  
