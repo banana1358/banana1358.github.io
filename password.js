266cef807055be41e899bd620f59fe94f766fe02
banana1358 <66886725+banana1358@users.noreply.github.com>
2020-06-27 13:36:50 -0700

    asdf

--- password.js
+++ password.js
@@ -1,6 +1,6 @@
 function passWord() {
   var password = prompt("Please Enter the password")
-  if(password == "password"){
+  if(password == ":)theasains(:"){
     alert("Welcome to the memories")
     window.open("memory.html")
     }
